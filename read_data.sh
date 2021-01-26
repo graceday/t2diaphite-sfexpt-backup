53a802051f9f0778e64b5808ac16fbf9c4ecccaf
graceday <45124592+graceday@users.noreply.github.com>
2021-01-26 16:19:51 +0000

    Add files via upload

--- read_data.sh
+++ read_data.sh
@@ -1,1 +1,1 @@
-python ./rd_data_3D.py --SFs $(cat scalefactors.txt) --SEQ $(cat sequences.txt) --pot "$1"
+python ./rd_data_3D.py --SFs $(cat scalefactors.txt) --SEQ $(awk '{print $1}' sequences.txt) --g $(awk '{print $2}' sequences.txt) --d $(awk '{print $3}' sequences.txt) --pot "$1"
